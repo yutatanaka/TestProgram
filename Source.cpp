9f46322cfd265e6dcd002e52921b2b9b4810d4a8
yutatanaka <koryotennis99@gmail.com>
2016-04-23 15:53:54 +0900

    ハノイの塔、プログラム作成開始

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main()
+{
+	int a = 1;
+	int b = 2;
+	int c = 3;
+
+	char stick[3] = {'A', 'B', 'C'};
+
+	// èoóÕ
+	printf("%c %c %c", stick[0], stick[1], stick[2]);
+
+	getchar();
+}
