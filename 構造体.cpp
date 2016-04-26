d3daddd0251c64c859388cdb72fc05908a2e2dd5
yutatanaka <koryotennis99@gmail.com>
2016-04-26 19:57:09 +0900

    構造体の作成

---
+++ 構造体.cpp
@@ -0,0 +1,22 @@
+
+// 構造体
+// 複数のデータ格納場所
+
+#include <stdio.h>
+
+struct Character
+{
+	int hp = 5;
+	int attack = 3;
+};
+
+// キャラクター構造体の変数を宣言
+Character player;
+
+
+void main()
+{
+	// 構造体をアクセスするには変数の後にドットを付ける
+	printf("hp : %d\n", player.hp);
+	getchar();
+}
