8a12367bcf72ed259e56ab5e8c3eba48e0b3617c
yutatanaka <koryotennis99@gmail.com>
2016-04-26 17:50:48 +0900

    型のサイズの確認

---
+++ 変数の型の大きさを調べる.cpp
@@ -0,0 +1,27 @@
+
+// Œ^‚Ì‘å‚«‚³‚ğ’²‚×‚é 
+// sizeof()ŠÖ”‚ğg‚¤
+
+#include <stdio.h>
+
+int main(void)
+{
+	// charŒ^
+	printf("char : %d\n", sizeof(char));
+	// short intŒ^
+	printf("short int : %d\n", sizeof(short int));
+	// intŒ^
+	printf("int : %d\n", sizeof(int));
+	// longŒ^
+	printf("long : %d\n", sizeof(long));
+	// floatŒ^
+	printf("float : %d\n", sizeof(float));
+	// doubleŒ^
+	printf("double : %d\n", sizeof(double));
+	// long doubleŒ^
+	printf("long double : %d\n", sizeof(long double));
+
+	getchar();
+
+	return 0;
+}
