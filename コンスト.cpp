9b2e8f7acfca67fd4d4bcadb1dfd09e5dbb14681
yutatanaka <koryotennis99@gmail.com>
2016-04-26 20:25:33 +0900

    コンストの使い道

--- コンスト.cpp
+++ コンスト.cpp
@@ -4,6 +4,16 @@
 
 #include <stdio.h>
 
+// クラス
+class Hoge
+{
+	const int* const func(const int a)const;
+	// constが先に書いてあれば、書き換え不能
+	// constが後に書いてあれば、差し替え不能
+	// constが引数の中で書いてあれば、引数が書き換えられない
+	// constが引数の後に書いてあれば、メンバ変数が書き換えられない
+};
+Hoge h;
 
 void main()
 {
@@ -14,6 +24,7 @@ void main()
 
 	int* const pa = &a;	// アドレスの再設定が出来ない
 	const int* pb = &b; // 中身を書き換えられない
+
 
 	getchar();
 }
