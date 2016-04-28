1bb0f4a1fa96f7971a4111083503d35ba758a76b
yutatanaka <koryotennis99@gmail.com>
2016-04-29 00:43:22 +0900

    if,else文

---
+++ if,else文.cpp
@@ -0,0 +1,27 @@
+
+// ifAelse•¶
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void main()
+{
+
+	int hoge;
+
+	printf("”’l‚ğ“ü—Í :");
+
+	scanf("%d", &hoge);
+
+	if (hoge >= 0)
+	{
+		printf("%d‚Í³‚Ì”‚Å‚·\n", hoge);
+	}
+	else
+	{
+		printf("%d‚Í•‰‚Ì”‚Å‚·\n", hoge);
+	}
+
+
+	getchar();
+}
