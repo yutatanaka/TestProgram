b497f9e2d2bf9e6e4f77dec63960d5b38d5c210d
yutatanaka <koryotennis99@gmail.com>
2016-05-01 18:55:32 +0900

    for文のテストプログラム

---
+++ for文.cpp
@@ -0,0 +1,21 @@
+
+// for•¶@”½•œ•¶
+
+#include <stdio.h>
+
+
+void main()
+{
+	int i;
+
+	// •Ï”AŒJ‚è•Ô‚·‰ñ”A‘‚â‚·”
+	// for•¶‚Å‚T‰ñŒJ‚è•Ô‚·
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+
+	getchar();
+}
