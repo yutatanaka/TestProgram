5134343733d36dae60ef874c4a179c5d9812f12c
yutatanaka <koryotennis99@gmail.com>
2016-05-09 18:12:10 +0900

    2重for文をつかったプログラム

---
+++ 2重for文.cpp
@@ -0,0 +1,20 @@
+
+// 2重for文を使ったプログラム
+
+#include <stdio.h>
+
+void main()
+{
+	int i, j;
+
+	for (i = 1; i <= 2; i++)
+	{
+		for (j = 1; j <= 3; j++)
+		{
+			printf("%d + %d = %d  ", i, j, i + j);
+		}
+		printf("\n");
+	}
+
+	getchar();
+}
