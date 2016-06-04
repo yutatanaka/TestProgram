5d3c15364659c2bf90f27bbbe6694eca5b05e43c
yutatanaka <koryotennis99@gmail.com>
2016-06-04 14:03:28 +0900

    ハノイの塔　C言語

--- hanoi.cpp
+++ hanoi.cpp
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+
 void Hanoi(int diskNumber, char *a, char *b, char *c)
 {
 	if (diskNumber >= 2)
@@ -15,7 +16,7 @@ void Hanoi(int diskNumber, char *a, char *b, char *c)
 
 	if (diskNumber >= 2)
 	{
-		Hanoi(diskNumber - 1, b, a, c);
+		Hanoi(diskNumber - 1, b, c, a);
 		
 	}
 	printf("%d ‚ð %s ‚©‚ç %s ‚ÉˆÚ“®\n", diskNumber, c, b);
@@ -27,7 +28,7 @@ void Hanoi(int diskNumber, char *a, char *b, char *c)
 void main()
 {
 
-	Hanoi(4, "A", "B", "C");
+	Hanoi(3, "A", "B", "C");
 
 	getchar();
 
